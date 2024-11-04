c37e27597ba10de418a81997acc3d1801a536904
afr2903 <afr102903@gmail.com>
2024-11-04 17:12:24 -0500

    [feat]: Modularization of the code (copilot help)

---
+++ fiber_camera.py
@@ -0,0 +1,133 @@
+"""Module to process video from camera to obtain the fiber diameter and display it"""
+import time
+import sys
+import cv2
+from PyQt5.QtWidgets import QWidget, QLabel, QImage, QPixmap, QDoubleSpinBox, QCheckBox, QTimer, QApplication
+
+from database import Database
+
+class FiberCamera(QWidget):
+    """Proceess video from camera to obtain the fiber diameter and display it"""
+    use_binary_for_edges = True
+    def __init__(self, target_diameter: QDoubleSpinBox) -> None:
+        super().__init__()
+        self.raw_image = QLabel()
+        self.processed_image = QLabel()
+        self.target_diameter = target_diameter
+        self.capture = cv2.VideoCapture(0)
+        self.line_value_updated = pyqtSignal(float)  # Create a new signal
+        self.diameter_coefficient = Database.get_calibration_data(
+            "diameter_coefficient")
+        self.previous_time = 0.0
+
+    def camera_loop(self) -> None:
+        """Loop to capture and process frames from the camera"""
+        success, frame = self.capture.read()
+        assert success, "Failed to capture frame"  # Check if frame is captured
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # To RGB for GUI
+        height, _, _ = frame.shape
+        frame = frame[height//4:3*height//4, :]  # Keep the middle section
+        edges, binary_frame = self.get_edges(frame)
+        # Get diameter from the binary image
+        # TODO: Tune and set to constants for fiber line detection
+        detected_lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50,
+                                         minLineLength=80, maxLineGap=20)
+        fiber_diameter = self.get_fiber_diameter(detected_lines)
+        # Plot lines on the frame
+        frame = self.plot_lines(frame, detected_lines)
+        # Emit the line_value_updated signal with the new line_value
+        #self.line_value_updated.emit(line_value)
+        Database.diameter_delta_time.append(time.time() - self.previous_time)
+        self.previous_time = time.time()
+        Database.diameter_readings.append(fiber_diameter)
+        Database.diameter_setpoint.append(self.target_diameter.value())
+
+        # Display the frame with lines
+        image_for_gui = QImage(frame, frame.shape[1], frame.shape[0],
+                                QImage.Format_RGB888)
+        self.raw_image.setPixmap(QPixmap(image_for_gui))
+
+        # Binary Image
+        image_for_gui = QImage(binary_frame, binary_frame.shape[1],
+                               binary_frame.shape[0], QImage.Format_Grayscale8)
+        self.processed_image.setPixmap(QPixmap(image_for_gui))
+
+    def get_edges(self, frame: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """Filter the frame to enhance the edges"""
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)  # Gray
+        kernel = np.ones((5,5), np.uint8)
+        frame = cv2.erode(frame, kernel, iterations=2)
+        frame = cv2.dilate(frame, kernel, iterations=1)
+        gaussian_blurred = cv2.GaussianBlur(frame, (5, 5), 0) 
+        threshold_value, binary_frame = cv2.threshold(
+            gaussian_blurred, 127, 255, cv2.THRESH_BINARY)
+        #print(f'Threshold value: {threshold_value}')
+
+        if FiberCamera.use_binary_for_edges is False:
+            edges = cv2.Canny(gray_frame, 100, 250, apertureSize=3)
+        else:
+            edges = cv2.Canny(binary_frame, 100, 250, apertureSize=3)
+        return edges, binary_frame
+
+    def get_fiber_diameter(self, lines):
+        """Get the fiber diameter from the edges detected in the image"""
+        leftmost_min = sys.maxsize
+        leftmost_max = 0
+        rightmost_min = sys.maxsize
+        rightmost_max = 0
+        if lines is None or len(lines) <= 1:
+            return 0
+        for line in lines:
+            x0, _, x1, _ = line[0]
+            # Find if local leftmost is less than the previous leftmost 
+            leftmost_min = min(leftmost_min, x0, x1)
+            leftmost_max = max(leftmost_max, min(x0, x1))
+            rightmost_min = min(rightmost_min, max(x0, x1))
+            rightmost_max = max(rightmost_max, x0, x1)
+
+        return (((leftmost_max - leftmost_min) + (rightmost_max - rightmost_min))
+                / 2 * self.diameter_coefficient )
+
+    def plot_lines(self, frame, lines):
+        """Plot the detected lines on the frame"""
+        if lines is not None:
+            for line in lines:
+                x0, y0, x1, y1 = line[0]
+                cv2.line(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
+        return frame
+
+    def calibrate(self):
+        """Calibrate the camera"""
+        num_samples = 20
+        accumulated_diameter = 0
+        average_diameter = 0
+        valid_samples = 0
+
+        for _ in range(num_samples):
+            success, frame = self.capture.read()
+            assert success, "Failed to capture frame"  # Check if frame is captured
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            edges, _ = self.get_edges(frame)
+            detected_lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 80,
+                                         minLineLength=60, maxLineGap=10)
+            fiber_diameter = self.get_fiber_diameter(detected_lines)
+            if fiber_diameter is not None:
+                accumulated_diameter += fiber_diameter
+                valid_samples += 1
+
+        if valid_samples > 0:
+            average_diameter = accumulated_diameter / valid_samples
+        
+        print(f"Average width of wire: {average_diameter} mm")
+
+        self.diameter_coefficient = 0.45/average_diameter
+        print(f"Diameter_coeff: {self.diameter_coefficient} mm")
+
+        Database.update_calibration_data("diameter_coefficient", 
+                                         str(self.diameter_coefficient))
+
+    def closeEvent(self, event):
+        """Close the camera when the window is closed"""
+        self.cap.release()
+        event.accept()
---
+++ spooler.py
@@ -0,0 +1,195 @@
+"""File to control the spooling process"""
+import time
+import numpy as np
+import RPi.GPIO as GPIO
+from gpiozero import RotaryEncoder
+
+from database import Database
+from user_interface import UserInterface
+
+
+class Spooler:
+    """DC Motor Controller for the spooling process"""
+    ENCODER_A_PIN = 24
+    ENCODER_B_PIN = 23
+    PWM_PIN = 5
+
+    PULSES_PER_REVOLUTION = 1176
+    READINGS_TO_AVERAGE = 10
+    SAMPLE_TIME = 0.1
+    DIAMETER_PREFORM = 7
+    DIAMETER_SPOOL = 15.2
+
+    def __init__(self, gui: UserInterface) -> None:
+        self.gui = gui
+        self.encoder = None
+        self.pwm = None
+        self.slope = Database.get_calibration_data("motor_slope")
+        self.intercept = Database.get_calibration_data("motor_intercept")
+        self.motor_calibration = True
+        if self.slope == -1 or self.intercept == -1:
+            self.motor_calibration = False
+        GPIO.setup(Spooler.PWM_PIN, GPIO.OUT)
+        self.initialize_encoder()
+        
+        # Control parameters
+        self.previous_time = 0.0
+        self.integral_diameter = 0.0
+        self.previous_error_diameter = 0.0
+        self.previous_steps = 0
+        self.integral_motor = 0.0
+        self.previous_error_motor = 0.0
+
+    def initialize_encoder(self) -> None:
+        """Initialize the encoder and SPI"""
+        self.encoder = RotaryEncoder(Spooler.ENCODER_A_PIN,
+                                     Spooler.ENCODER_B_PIN, max_steps=0)
+
+    def start(self, frequency: float, duty_cycle: float) -> None:
+        """Start the DC Motor PWM"""
+        self.pwm = GPIO.PWM(Spooler.PWM_PIN, frequency)
+        self.pwm.start(duty_cycle)
+
+    def stop(self) -> None:
+        """Stop the DC Motor PWM"""
+        if self.pwm:
+            self.pwm.stop()
+
+    def update_duty_cycle(self, duty_cycle: float) -> None:
+        """Update the DC Motor PWM duty cycle"""
+        self.pwm.ChangeDutyCycle(duty_cycle)
+
+    def get_average_diameter(self) -> float:
+        """Get the average diameter of the fiber"""
+        if len(Database.diameter_readings) < Spooler.READINGS_TO_AVERAGE:
+            return (sum(Database.diameter_readings) /
+                    len(Database.diameter_readings))
+        else:
+            return (sum(Database.diameter_readings[-Spooler.READINGS_TO_AVERAGE:])
+                    / Spooler.READINGS_TO_AVERAGE)
+
+    def diameter_to_rpm(self, diameter: float) -> float:
+        """Convert the fiber diameter to RPM of the spooling motor"""
+        stepper_rpm = self.gui.extrusion_motor_speed.value()
+        return 25/28 * 11 * stepper_rpm * (Spooler.DIAMETER_PREFORM**2 /
+                                        (Spooler.DIAMETER_SPOOL * diameter**2))
+
+    def rpm_to_duty_cycle(self, rpm: float) -> float:
+        """Convert the RPM to duty cycle"""
+        return self.slope * rpm + self.intercept
+
+    def motor_control_loop(self, current_time: float) -> None:
+        """Closed loop control of the DC motor for desired diameter"""
+        if current_time - self.previous_time <= Spooler.SAMPLE_TIME:
+            return
+        try:
+            if not self.motor_calibration:
+                self.gui.show_message("Motor calibration data not found",
+                                    "Please calibrate the motor.")
+                self.motor_calibration = True
+            target_diameter = self.gui.target_diameter.value()
+            current_diameter = self.get_average_diameter()
+
+            diameter_ku = self.gui.diameter_gain.value()
+            diameter_tu = self.gui.diameter_oscilation_period.value()
+            diameter_kp = 0.6 * diameter_ku
+            diameter_ti = diameter_tu / 2
+            diameter_td = diameter_tu / 8
+            diameter_ki = diameter_kp / diameter_ti
+            diameter_kd = diameter_kp * diameter_td
+
+            motor_ku = self.gui.motor_gain.value()
+            motor_tu = self.gui.motor_oscilation_period.value()
+            motor_kp = 0.6 * motor_ku
+            motor_ti = motor_tu / 2
+            motor_td = motor_tu / 8
+            motor_ki = motor_kp / motor_ti
+            motor_kd = motor_kp * motor_td
+
+            delta_time = current_time - self.previous_time
+            self.previous_time = current_time
+            error = target_diameter - current_diameter
+            self.integral_diameter += error * delta_time
+            self.integral_diameter = max(min(self.integral_diameter, 0.5), -0.5)
+            derivative = (error - self.previous_error_diameter) / delta_time
+            self.previous_error_diameter = error
+            output = (diameter_kp * error + diameter_ki * self.integral_diameter
+                      + diameter_kd * derivative)
+            setpoint_rpm = self.diameter_to_rpm(target_diameter)
+            setpoint_rpm = max(min(setpoint_rpm, 0), 60)
+
+            # Control the motor
+            delta_steps = self.encoder.steps - self.previous_steps
+            self.previous_steps = self.encoder.steps
+            current_rpm = (delta_steps / Spooler.PULSES_PER_REVOLUTION * 
+                           60 / delta_time)
+            error = setpoint_rpm - current_rpm
+            self.integral_motor += error * delta_time
+            self.integral_motor = max(min(self.integral_motor, 100), -100)
+            derivative = (error - self.previous_error_motor) / delta_time
+            self.previous_error_motor = error
+            output = (motor_kp * error + motor_ki * self.integral_motor +
+                        motor_kd * derivative)
+            output_duty_cycle = self.rpm_to_duty_cycle(output) 
+            output_duty_cycle = max(min(output_duty_cycle, 100), 0)
+            self.update_duty_cycle(output_duty_cycle)
+
+            # Update plots
+            self.gui.motor_plot.update_plot(current_time, current_rpm,
+                                            setpoint_rpm)
+            self.gui.diameter_plot.update_plot(current_time, current_diameter,
+                                                  target_diameter)
+
+            # Add data to the database
+            Database.spooler_delta_time.append(delta_time)
+            Database.spooler_setpoint.append(setpoint_rpm)
+            Database.spooler_rpm.append(current_rpm)
+            Database.spooler_gain.append(diameter_ku)
+            Database.spooler_oscilation_period.append(diameter_tu)
+        except Exception as e:
+            print(f"Error in motor control loop: {e}")
+            self.gui.show_message("Error", "Error in motor control loop",
+                                  "Please restart the program.")
+
+    def calibrate(self) -> None:
+        """Calibrate the DC Motor"""
+        rpm_values = []
+        duty_cycles = []
+        num_samples = 5
+
+        try:
+            for duty_cycle in range(20, 101, 10):  # Sweep duty cycle from 0% to 100% in increments of 10%
+                rpm_samples = []
+                for _ in range(num_samples):
+                    self.update_duty_cycle(duty_cycle)
+                    time.sleep(2)
+                    # Measure RPM
+                    oldtime = time.perf_counter()
+                    oldpos = self.encoder.steps
+                    time.sleep(Spooler.SAMPLE_TIME)
+                    newtime = time.perf_counter()
+                    newpos = self.encoder.steps
+                    dt = newtime - oldtime
+                    ds = newpos - oldpos
+                    rpm = ds / Spooler.PULSES_PER_REVOLUTION / dt * 60
+                    rpm_samples.append(rpm)
+                avg_rpm = sum(rpm_samples) / num_samples
+                duty_cycles.append(duty_cycle)
+                rpm_values.append(avg_rpm)
+                print(f"Duty Cycle: {duty_cycle}% -> Avg RPM: {avg_rpm:.2f}")
+
+            # Fit a curve to the data
+            coefficients = np.polyfit(rpm_values, duty_cycles, 1)
+            self.slope = coefficients[0]
+            self.intercept = coefficients[1]
+            Database.update_calibration_data("motor_slope", str(self.slope))
+            Database.update_calibration_data("motor_intercept", str(self.intercept))
+
+        except KeyboardInterrupt:
+            print("\nData collection stopped\n\n")
+
+        self.gui.show_message("Motor calibration completed.",
+                               "Please restart the program.")
+        self.stop()
+        self.previous_steps = self.encoder.steps
+        print("aaaa")
